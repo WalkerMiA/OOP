85b651878850a16e95d1552c5ae0998c08dba5a5
WalkerMiA <michaelwalker110100@gmail.com>
2025-10-24 19:39:33 -0500

    Add files via upload

--- Project 4.py
+++ Project 4.py
@@ -1,3 +1,4 @@
+import pickle
 class Customer:
     def __init__(self):
         self.cid = ""
@@ -7,7 +8,7 @@ class Customer:
         self.email_id = ""
         self.balance = 0.0
         self.credit_card = []
-        self.debit_card = ""
+        self.debit_card = []
 
     def add_values_to_customer(self):
         self.cid = input("Enter Customer ID:")
@@ -32,14 +33,16 @@ class Customer:
         print("Phone Number:", self.phone)
         print("Email:", self.email_id)
         print("Balance:", self.balance)
-        for x in self.credit_card:
-            print("Credit Card(s):", x.card_num)
-        print("Debit Card:", self.debit_card)
+        for card in self.credit_card:
+            print("Credit Card(s):", card.card_num)
+        for debt in self.debit_card:
+            print("Debit Card(s):", debt.card_num)
 
     def add_credit_card(self, ccard):
         self.credit_card.append(ccard)
     def add_debit_card(self, dcard):
-        self.debit_card += dcard
+        self.debit_card.append(dcard)
+
 
 class Card:
     def __init__(self):
@@ -60,26 +63,80 @@ class Card:
         print("Expiration Date:", self.expiration)
         print("Balance:", self.balance)
 
-c1 = Customer()
-c1.add_values_to_customer()
-card1 = Card()
-card1.add_card()
-card1.display_card_info()
-card2 = Card()
-card2.add_card()
-card2.display_card_info()
-c1.add_debit_card(card1)
-c1.add_credit_card(card2)
-c1.display_cust_info()
+customers = []
+cards = []
 
-c2 = Customer()
-c2.add_values_to_customer()
+def find_customer(customer_id):
+    for x in customers:
+        if x.cid == customer_id:
+            return x
+    return None
+while 1:
+    print("1. Create Customer")
+    print("2. Create Card")
+    print("3. Transfer Funds")
+    print("4. Assign Cards")
+    print("5. Display Customer Info")
+    print("6. Display Card Info")
+    print("7. Commit")
+    print("8. Exit")
 
+    option = input("Enter Option:")
 
-d = float(input("Enter amount to debit:"))
-c1.debit_from(d)
-c1.display_cust_info()
+    if option == "1":
+        new_customer = Customer()
+        new_customer.add_values_to_customer()
+        customers.append(new_customer)
+    elif option == "2":
+        new_card = Card()
+        new_card.add_card()
+        cards.append(new_card)
+    elif option == "3":
+        sender_id = input("Enter Customer ID:")
+        recipient_id = input("Enter Customer ID:")
+        sender = find_customer(sender_id)
+        recipient = find_customer(recipient_id)
 
-c = float(input("Enter amount to credit:"))
-c2.credit_to(c)
-c2.display_cust_info()
+        if sender and recipient:
+            amount = float(input("Enter Amount to Transfer:"))
+            sender.debit_from(amount)
+            recipient.credit_to(amount)
+        else:
+            print("One or both customer IDs do not match.")
+    elif option == "4":
+        cid = input("Enter Customer ID:")
+        customer = find_customer(cid)
+        if customer:
+            card_type = input("Credit or Debit:").lower()
+            for x in cards:
+                if x.card_type.lower() == card_type:
+                    x.display_card_info()
+
+            selected_card_num = input("Enter Card Number:")
+            for x in cards:
+                if x.card_num == selected_card_num and x.card_type.lower() == card_type:
+                    if card_type == "debit":
+                        customer.add_debit_card(x)
+                    elif card_type == "credit":
+                        customer.add_credit_card(x)
+                    break
+            else:
+                print("Card not found.")
+        else:
+            print("Customer not found.")
+    elif option == "5":
+        cust = input("Enter Customer ID:")
+        for x in customers:
+            if x.cid == cust:
+                x.display_cust_info()
+    elif option == "6":
+        card_num = input("Enter Card Number:")
+        for x in cards:
+            if x.card_num == card_num:
+                x.display_card_info()
+    elif option == "7":
+        f1 = open("cust_and_cards.dat", "wb")
+        pickle.dump({"customers": customers, "cards": cards}, f1)
+        f1.close()
+    elif option == "8":
+        break
