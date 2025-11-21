aba99f0c938e8dac87df3235247c197bdb74c2e9
WalkerMiA <michaelwalker110100@gmail.com>
2025-11-21 00:56:44 -0600

    Add files via upload

--- Semester_Project_client.py
+++ Semester_Project_client.py
@@ -1,10 +1,31 @@
 import socket
 
+#Connect to the server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 9999))
 
-message = client.recv(1024).decode()
-client.send(input(message).encode())
-message = client.recv(1024).decode()
-client.send(input(message).encode())
-print(client.recv(1024).decode()) #Will receive whether login was successful or not
+def send_and_receive(prompt=True):
+    message = client.recv(1024).decode()
+    if prompt:
+        response = input(message + " ") #Shows the message as a prompt
+        client.send(response.encode())
+    else:
+        print(message) #Prints message without responding
+
+#Get Action (login or signup)
+send_and_receive() #Do you want to login or signup?
+
+#Receives and responds based on the server prompts
+while True:
+    try:
+        message = client.recv(1024).decode()
+
+        #final success or fail message - print and break
+        if "Login Successful" in message or "Login Failed" in message or "Signup Successful" in message or "Signup Failed" in message:
+            print(message)
+            break
+        response= input(message + " ")
+        client.send(response.encode())
+    except KeyboardInterrupt:
+        print("\nClient closed")
+        break
